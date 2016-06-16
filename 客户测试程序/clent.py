1376096c32c4adb921c63bcf23e35c5b7b1bc81e
chao <531236305@qq.com>
2016-06-16 18:09:28 +0800

    todo

--- 客户测试程序/clent.py
+++ 客户测试程序/clent.py
@@ -7,21 +7,36 @@ from multiprocessing import Process
 import select
 import time
 
-def epoll_loop(epoller, fd_to_socket, fd_connect_flags, fd_to_times):
+def epoll_loop(epoller, fd_to_socket, fd_to_times):
     """ 在epoller上轮询"""
+
+    # 记录所有socket的状态：1为connneting，2为sending，3为recving
+    fd_state = {fd: 1 for fd in fd_to_socket.keys()} # 所有socket的初始状态为1
+
+    # 和fd相关的消息，类型为{fd:bytes}，当socket状态为sending时，msg[fd]为发送消息，当socket的状态为recving时，
+    # msg[fd]为接收消息。
+    msg = {fd: b'' for fd in fd_to_socket.keys()}
+
     while True:
         events = epoller.poll()
-        for fd, envent in events:
+        for fd, event in events:
             # 如果是连接完成
-            if event == select.EPOLLIN and fd_connect_flags[fd] == False:
-                fd_connect_flags[fd] == True
+            if event == select.EPOLLIN and fd_state[fd] == 1:
                 # 记录连接完成的时间
                 fd_to_times[fd]['connect_completed_time'] = round(time.time() * 1000)
-                # todo:把信息转为json字符串发送给服务器
+                # todo:把times信息打包发送给服务器
 
-            # 如果是需要读取信息
-            elif event == select.EPOLLIN and fd_connect_flags[fd] == True:
-                # TODO:读取信息
+                # 将fd重新注册为关心可写事件
+                epoller.modify(fd, select.EPOLLOUT)
+                #状态转为sending
+                fd_state[fd] = 2
+
+            # 如果是可以发送消息
+            elif event == select.EPOLLOUT and fd_state[fd] == 2 :
+                
+            # 如果是需要接收信息
+            elif event == select.EPOLLIN and fd_state[fd] == 3:
+                # TODO:接收信息
 
 
 def mult_connect_to_server(ip, port, conn_num):
@@ -44,12 +59,9 @@ def mult_connect_to_server(ip, port, conn_num):
 
     # 记录文件描述符号到socket对象的映射
     fd_to_socket = {sock.fileno:sock for sock in sockets}
-    
-    #记录fd对应的socket是否已完成连接
-    fd_connect_flags = {sock.fileno:False for sock in sockets}
 
     # 轮询监听
-    epoll_loop(epoller, fd_to_socket, fd_connect_flags, fd_to_times)
+    epoll_loop(epoller, fd_to_socket, fd_to_times)
 
 
 def stress_test(server_ip, server_port, conn_num):
