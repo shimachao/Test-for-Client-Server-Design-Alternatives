4452eb665929eb2775789bf5119be58bbe413a4e
chao <531236305@qq.com>
2016-06-17 18:04:02 +0800

    初步实现

--- 迭代服务器程序/iterative_server.py
+++ 迭代服务器程序/iterative_server.py
@@ -3,7 +3,33 @@ import socket
 import sys
 import josn
 
-def main(ip, port):
+def complete_recv(conn_socket):
+    """ 将conn_socket上的数据读完,
+    将接收到的所有数据用一个bytes对象返回"""
+    msg = b''
+    while True:
+        bs = conn_socket.recv(1024)
+        if len(bs) == 0:
+            # 如果对方关闭了连接
+            conn_socket.close()
+            # todo:抛出异常，表示未意料到的关闭
+        msg += bs
+        if bs[-1] == ord('\r'):
+            # 如果对方发送完数据
+           break
+    return msg
+
+def complete_send(conn_socket, msg):
+    """ 将msg通过conn_socket发送出去"""
+    while True:
+        count = conn_socket.send(msg)
+        msg = msg[count:]  # 去掉已发送的部分
+        if len(msg) == 0:
+            # 如果数据发送完毕
+            break
+
+
+def server(ip, port):
     """ 迭代处理客户端的连接请求"""
     
     listen_socket = socket.socket(family=AF_INET, type=SOCK_STREAM)
@@ -12,12 +38,19 @@ def main(ip, port):
     listen_socket.listen(10)
     
     while True:
+        # 接受一个连接
         conn_socket, addr = listen_socket.accept()
+        # 读取客户端发过来的数据
+        msg = complete_recv(conn_socket)
+        # 将数据原封不动发送回去
+        complete_send(conn_socket, msg)
+        conn_socket.close()
+
     
 if __name__ == '__main__':
     if len(sys.argv < 3):
         print("缺少IP和端口号参数\n")
         return;
      else:
-         main(sys.argv[1], sys.argv[2])
+         server(sys.argv[1], sys.argv[2])
     
