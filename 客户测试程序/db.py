80e7eb152a96ad2345c9e4048af4045d7e2c86b0
chao <531236305@qq.com>
2016-06-17 12:55:28 +0800

    初步实现db模块

--- 客户测试程序/db.py
+++ 客户测试程序/db.py
@@ -1,7 +1,66 @@
 # coding:utf-8
 
-import postgresql
+import os
+import psycopg2 as pg
 
 
 class DB():
     """ 用于管理对数据库的访问"""
+
+    def __init__(self, db_name, table_name):
+        """ 连接到dbname数据库"""
+
+        # 从环境变量中获取用户名、密码、数据库名
+        user = os.getenv('db_user')
+        if not user:
+            print('环境变量中没有设置访问数据库需要的用户名\n')
+ 
+        passwd = os.getenv('db_password')
+        if not passwd:
+            print('环境变量中没有设置数据库密码\n')
+
+        db = os.getenv('db_name')
+        if not db:
+            print('环境变量中没有设置要访问的数据库名称\n')
+
+        self.user = user
+        self.passwd = passwd
+        self.db_name = db_name
+        self.table_name = table_name
+        self.conn = pg.connect(user=user, password=passwd, database=db, host='localhost', port=5432)
+        self.cursor = self.conn.cursor()
+    
+    def create_table(table_name=None):
+        """ 创建表"""
+        if table_name:
+            self.table_name = table_name
+
+        sql_str = """CREATE TABLE %s (
+                    id  SERIAL PRIMARY KEY,
+                    connect_time bigint NOT NULL,
+                    connected_completed_time bigint NOT NULL,
+                    request_completed_time bigint NOT NULL
+                    )""" % self.table_name
+        self.cursor.execute(sql_str)
+        self.conn.commit()
+
+    def insert(self, connect_time, connected_completed_time,request_completed_time):
+        """ 向数据库中插入数据"""
+        sql_str = """INSERT INTO %s(connect_time, connected_completed_time,request_completed_time) VALUES(%d, %d, %d)""" % 
+        (self.table_name, connect_time, connected_completed_time, request_completed_time)
+
+    def __del__(self):
+        if not self.conn:
+            self.conn.commit()
+            if not self.cursor:
+                self.cursor.close()
+            self.conn.close()
+        
+
+# 测试
+if __name__ == '__main__':
+    import time
+    table_name = input("输入表名")
+    db = DB(db_name='test_data_db', table_name=table_name)
+    db.create_table()
+    t = round(time.time()*1000)
