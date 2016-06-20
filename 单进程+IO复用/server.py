1f501eff4d996e91103b4183f6fec95ce1e9a23a
chao <531236305@qq.com>
2016-06-21 04:47:33 +0800

    我对自己很失望

--- 单进程+IO复用/server.py
+++ 单进程+IO复用/server.py
@@ -2,6 +2,7 @@
 import socket
 import sys
 import select
+import signal
 from select import epoll
 
 
@@ -73,17 +74,18 @@ class SocketIO():
             self.msgs.pop(fd)
 
 
-def epoll_loop(epoller, listen_socket_fd):
+def epoll_loop(epoller, listen_socket):
     """ 监听listen_socket 如果收到新的连接就把新的连接也加入监控"""
 
     io_hander = SocketIO(epoller)  # 专门处理io的对象
+    listen_socket_fd = listen_socket.fileno()
     while  True:
         events = epoller.poll()
         for fd, flag in events:
             # 如果是listen socket上的读事件
             if fd == listen_socket_fd and flag == select.EPOLLIN:
                 # 接受新连接
-                conn_socket,addr = sockets[listen_socket_fd].accept()
+                conn_socket,addr = listen_socket.accept()
                 # 将新连接加入监听计划
                 io_hander.add(conn_socket)
 
@@ -124,11 +126,11 @@ def server(ip, port):
     signal.signal(signal.SIGINT, sigint_handler)
 
     # 开始epoll轮询
-    epoll_loop(epoller, listen_socket.fileno(), sockets)
+    epoll_loop(epoller, listen_socket)
 
 
 if __name__ == '__main__':
-    if len(sys.argv < 3):
+    if len(sys.argv) < 3:
         print("缺少IP和端口号参数\n")
         sys.exit()
     print('serving at', sys.argv[1], sys.argv[2])
